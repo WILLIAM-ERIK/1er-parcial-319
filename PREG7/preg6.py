44ee5ba2624a5fe4e9cc4e7c3a3b2886af39af61
WILLIAM-ERIK <86015215+WILLIAM-ERIK@users.noreply.github.com>
2022-10-04 20:21:23 -0400

    Add files via upload

---
+++ PREG7/preg6.py
@@ -0,0 +1,14 @@
+print("Ingrese el primer numero")
+n1=float(input())
+print("Ingrese el segundo numero")
+n2=float(input())
+
+suma= n1+n2
+resta= n1-n22
+multi= n1*n2
+div= n1/n2
+
+print("la suma es:",suma)
+print("la resta es:",resta)
+print("la multuiplicacion es:",multi)
+print("la division es:",div)
